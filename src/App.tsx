6f3a880e8a2960d8f913c2ec6c2dfc2d05517c03
Танаев Роман <tanaevr@gmail.com>
2020-11-02 12:51:43 +0300

    Add ESlint && Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
-import './static/custom.css';
 
-const s = require('./static/App.modules.scss');
+const s = require('./static/App.module.scss');
 
 const App: FC = () => {
-    return (
-        <div className={cn(s.default.header, 'custom')}>App</div>
-    )
-}
+  return <div className={cn(s.default.header, 'custom')}>App</div>;
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,55 @@ const HTMLWebpachPlugins = require('html-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-    },
-    mode: NODE_ENV ? NODE_ENV : 'development',
-    entry: path.resolve(__dirname, 'src/index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(t|j)sx?$/,
-                use: ['ts-loader'],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-modules-typescript-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
+              },
             },
-            {
-                test: /\.(s*)css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]__[hash:base64:5]',
-                                auto: /\.modules\.\w+$/i
-                            },
-                        }
-                    },
-                    'sass-loader'
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpachPlugins({
-            template: path.resolve(__dirname, 'index.html'),
-        })
+          },
+          'sass-loader',
+        ],
+      },
     ],
-    devServer: {
-        port: 3000,
-        open: false,
-        hot: true,
-    },
-    devtool: 'source-map'
+  },
+  plugins: [
+    new HTMLWebpachPlugins({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: false,
+    hot: true,
+  },
+  devtool: 'source-map',
 };
